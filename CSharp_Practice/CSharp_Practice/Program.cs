2b61947c4febb541342cd2468721bb1c7d2e2281
arafat-bin-reza <arafat.reza.developer@gmail.com>
2022-02-09 05:18:22 +0600

    CSharp_Practice added

---
+++ CSharp_Practice/CSharp_Practice/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //C# Tutorial - Introduction
+
+            //Console.WriteLine("Welcome to C# Training!");
+            //Main1();
+
+
+            //C# Tutorial - Reading and writing to a console
+
+            //Console.WriteLine("Please enter your name");
+
+            //var username = Console.ReadLine();
+
+            //Console.WriteLine("Please enter your first name");
+
+            //var firstname = Console.ReadLine();
+
+            //Console.WriteLine("Please enter your last name");
+
+            //var lastname = Console.ReadLine();
+
+            //Concatenate name with hello word and print
+            //Console.WriteLine("Hello " + username);
+
+            //Placeholder syntax to print name with hello word - Most Preferred
+            //Console.WriteLine("Hello {0}", username);
+            //Console.WriteLine("Hello {0} {1}", firstname, lastname);
+
+
+            //Note: C# is case sensitive
+
+            //C# Tutorial - Built - in types
+
+            //Console.WriteLine("Min = {0}", int.MinValue);
+            //Console.WriteLine("Max = {0}", int.MaxValue);
+
+
+            //C# Tutorial - String type in c#
+
+            //Escape Sequence
+
+            //string a = "\"arafat\"";
+            //string b = "one\ntwo\nthree";
+            //string c = "D:\\Web Development\\aspdotnet_practice";
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+
+            //Verbatim Literal
+
+            //string d = @"D:\Web Development\aspdotnet_practice";
+            //Console.WriteLine(d);
+
+
+            //C# Tutorial - Common Operators in c#
+
+            //int a = 10;
+            //int b = 2;
+            //int c = 0;
+
+            //bool number10 = a == 10 ? true : false;
+
+            //Console.WriteLine("Number 10 is {0}", number10);
+
+
+            //if (a==10 && b == 2)
+            //{
+            //    c = a / b;
+            //    Console.WriteLine(c);
+            //}
+
+            
+
+        }
+
+        static void Main1()
+        {
+            Console.WriteLine("Welcome to C# Training 1!");
+        }
+    }
+}
