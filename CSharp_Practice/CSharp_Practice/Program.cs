d7b3b941f522ab72f64f0a2db98a76a7ca8382a0
arafat-bin-reza <arafat.reza.developer@gmail.com>
2022-02-12 04:30:35 +0600

    Datatypes conversion and Arrays added

--- CSharp_Practice/CSharp_Practice/Program.cs
+++ CSharp_Practice/CSharp_Practice/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Namespace Declaration
+using System;
 
 namespace CSharp_Practice
 {
@@ -9,6 +10,7 @@ namespace CSharp_Practice
 
             //C# Tutorial - Introduction
 
+            //Write to console
             Console.WriteLine("Welcome to C# Training!");
             Main1();
 
@@ -17,8 +19,10 @@ namespace CSharp_Practice
 
             //Example 1
 
+            //Prompt the user for his name
             Console.WriteLine("Please enter your name");
 
+            //Read the name from console
             var username = Console.ReadLine();
 
             //Concatenate name with hello word and print
@@ -53,7 +57,7 @@ namespace CSharp_Practice
 
             string a = "\"arafat\"";
             string b = "one\ntwo\nthree";
-            string c = "D:\\Web Development\\aspdotnet_practice";
+            string c = "D:\\Web Development\\aspdotnet_practice"; //Without Verbatim Literal - Less Readable
 
             Console.WriteLine(a);
             Console.WriteLine(b);
@@ -61,7 +65,7 @@ namespace CSharp_Practice
 
             //Verbatim Literal
 
-            string d = @"D:\Web Development\aspdotnet_practice";
+            string d = @"D:\Web Development\aspdotnet_practice"; //With Verbatim Literal - Better Readable
             Console.WriteLine(d);
 
 
@@ -71,7 +75,7 @@ namespace CSharp_Practice
             int b2 = 2;
             int c2 = 0;
 
-            bool number10 = (a2 == 10) ? true : false;
+            bool number10 = (a2 == 10) ? true : false; //Ternary Operator
 
             Console.WriteLine("Number 10 is {0}", number10);
 
@@ -136,6 +140,122 @@ namespace CSharp_Practice
             int AvailableTickets2 = TicketOnSale2 ?? 0;
 
             Console.WriteLine("Available Tickets: {0}", AvailableTickets2);
+
+            //C# Tutorial Datatypes conversion
+
+            //implicit conversion
+
+            //Example - 1
+
+            int i = 100;
+
+            //Float is a bigger datatype than int. So, No loss of data and Exceptions. Hence implicit conversion
+            float f = i;
+
+            Console.WriteLine(f);
+
+            //explicit conversion
+
+            //Example - 2
+
+            float f2 = 125.43F; //Cannot implicitly convert float to int. Fractional part will be lost.
+                                //Float is a bigger datatype than int, so there is also a possibility of overflowException
+                                //int i = f;
+
+            //int i2 = (int)f2;  //use type cast () operator for explicit conversion, type cast () operator never throw exception if it can not convert that float to int
+
+            int i2 = Convert.ToInt32(f2);  //use Convert class of DotNet for explicit conversion, 
+                                           //Convert class throw OverFlowException if that float value is too large or small to convert into int.
+                                           //OverflowException: The exception that is thrown when an arithmetic, casting, or conversion operation in a checked context results in an overflow.
+                                           //An OverflowException is thrown at run time, so this is a runtime exception.
+
+            Console.WriteLine(i2);
+
+
+            //Parse method () - Use Parse() if you are sure the value will be valid, otherwise use TryParse()
+
+            //Example - 3
+
+            string strNumber = "100";
+
+            int i3 = int.Parse(strNumber);
+
+            Console.WriteLine(i3);
+
+            //Example - 4
+
+            string strNumber2 = "100TG";
+
+            int i4 = int.Parse(strNumber2); //FormatException: Input string was not in a correct format.
+                                            //Parse method () throw an exception if it cannot parse the value
+                                            //FormatException: The exception that is thrown when the format of an argument is invalid, or when a composite format string is not well formed.
+                                            //An FormatException is thrown at run time, so this is a runtime exception.
+
+
+            Console.WriteLine(i4);
+
+
+            //TryParse method ()
+
+            //Example - 5
+
+            string strNumber3 = "100TG";
+
+            int Result = 0;
+
+            bool IsConversionSuccessful = int.TryParse(strNumber3, out Result); //TryParse method () returns a bool indication whether it succeeded or failed
+                                            
+            if(IsConversionSuccessful)
+            {
+                Console.WriteLine(Result);
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid Number");
+            }
+
+            //C# Tutorial - Arrays in C#
+            //Arrays: An array is a collection of similar data types.
+
+            //Example - 1
+
+            //Initialize and Assign Values in difference lines
+            int[] EvenNumbers = new int[3];
+
+            EvenNumbers[0] = 0;
+            EvenNumbers[1] = 2;
+            EvenNumbers[2] = 4;
+            EvenNumbers[3] = 6; //IndexOutofRangeException: Index was out the bounds of the array.
+                                //IndexOutOfRangeException: The exception that is thrown when an attempt is made to access an element of an array or collection with an index that is outside its bounds.
+                                //The IndexOutOfRangeException is a Runtime Exception thrown only at runtime.
+
+            Console.WriteLine(EvenNumbers[2]);
+
+            //Initialize and Assign Values in difference lines
+
+            //Example - 2
+
+            int[] OddNumbers = { 1,3,5 };
+
+            Console.WriteLine(OddNumbers[2]);
+
+
+            //Example - 3
+
+            //Initialize and Assign Values in difference lines
+            int[] EvenNumbers2 = new int[3];
+
+            EvenNumbers2[0] = 0; //EvenNumbers2[0] this [0] is called integral indices.
+            EvenNumbers2[1] = 2;
+
+            Console.WriteLine(EvenNumbers[20]); //IndexOutofRangeException: Index was out the bounds of the array.
+                                                //IndexOutOfRangeException: The exception that is thrown when an attempt is made to access an element of an array or collection with an index that is outside its bounds.
+                                                //The IndexOutOfRangeException is a Runtime Exception thrown only at runtime.
+
+
+            //Advantages: Arrays are strongly typed.
+            //Disadvantages: Arrays cannot grows in size once initialized.
+            //Have to rely on integral indices to store or retrive items from the array.
 
         }
 
