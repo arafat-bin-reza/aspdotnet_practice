49b0770392710cd33a8c02df95162a15e18094e9
arafat-bin-reza <arafat.reza.developer@gmail.com>
2022-06-26 22:57:51 +0600

    Some new changes on existing code

---
+++ CSharp_Practice/CSharp_Practice2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+        }
+    }
+}
--- CSharp_Practice/Introduction/Program.cs
+++ CSharp_Practice/Introduction/Program.cs
@@ -1,21 +1,18 @@
 ﻿//Namespace Declaration
 using System;
 
-namespace Introduction
-{
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //C# Tutorial - Introduction
 
             //Write to console
             Console.WriteLine("Welcome to C# Training!");
             Main1();
-        }
+    }
         static void Main1()
         {
             Console.WriteLine("Welcome to C# Training 1!");
         }
-    }
 }
